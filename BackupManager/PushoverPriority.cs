f4e6b74159b08d8da394dec2c3ae9e65e0c607ad
Andy Reeves <23584383+andy-reeves@users.noreply.github.com>
2021-10-27 13:26:09 +0100

    Add support for PushoverPriority. Add values for minimum MasterFolder space before raaising an alert

---
+++ BackupManager/PushoverPriority.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupManager
+{
+    public enum PushoverPriority
+    {
+        Low = -1,
+        Normal = 0,
+        High = 1
+    }
+}
+
