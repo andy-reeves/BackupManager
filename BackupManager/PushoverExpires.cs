87ddef1dc342ca2a056cf6d56a72f0997d79b67b
Andy Reeves <andy@pb.eu>
2023-06-07 12:56:27 +0100

    Add pushoverexpires and pushoverretries to API

---
+++ BackupManager/PushoverExpires.cs
@@ -0,0 +1,12 @@
+﻿namespace BackupManager
+{
+    public enum PushoverExpires
+    {
+        Immediately = 0,
+        ThirtySeconds = 30,
+        OneMinute = 60,
+        FiveMinutes = 300,
+        ThirtyMinutes = 1800,
+        OneHour = 3600
+    }
+}
