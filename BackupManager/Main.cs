8e1c626179ca0a791628bedb6e6ea00e2d0fa96a
Andy Reeves <andy@pb.eu>
2023-06-08 15:03:51 +0100

    Update copy messages

--- BackupManager/Main.cs
+++ BackupManager/Main.cs
@@ -457,7 +457,7 @@ namespace BackupManager
                                                           mediaBackup.PushoverAppToken,
                                                           logFile,
                                                           BackupAction.BackupFiles,
-                                                          $"Skipping {sourceFileName} as not enough free space for {sourceFileSize}"
+                                                          $"[{fileCounter}/{totalFileCount}] Skipping {sourceFileName} as not enough free space for {sourceFileSize}"
                                                           );
                                     outOfDiskSpaceMessageSent = true;
                                 }
