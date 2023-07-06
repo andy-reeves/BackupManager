49e2daf03aca44ea2fd2e44c15e34ce8fbb403fa
Andy Reeves <andy@pb.eu>
2023-07-06 10:53:29 +0100

    Return False is hashes have changed and log issue Add ScheduledBackupStartTime to config Remove the minutes ot next run of backup and add a startTime

---
+++ BackupManager/DailyTrigger.cs
@@ -0,0 +1,71 @@
+﻿namespace BackupManager
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+
+    /// <summary>
+    /// Utility class for triggering an event every 24 hours at a specified time of day
+    /// </summary>
+    public class DailyTrigger : IDisposable
+    {
+        /// <summary>
+        /// Time of day (from 00:00:00) to trigger
+        /// </summary>
+        TimeSpan TriggerHour { get; }
+
+        /// <summary>
+        /// Task cancellation token source to cancel delayed task on disposal
+        /// </summary>
+        CancellationTokenSource CancellationToken { get; set; }
+
+        /// <summary>
+        /// Reference to the running task
+        /// </summary>
+        Task RunningTask { get; set; }
+
+        /// <summary>
+        /// Initiator
+        /// </summary>
+        /// <param name="hour">The hour of the day to trigger</param>
+        /// <param name="minute">The minute to trigger</param>
+        /// <param name="second">The second to trigger</param>
+        public DailyTrigger(int hour, int minute = 0, int second = 0)
+        {
+            TriggerHour = new TimeSpan(hour, minute, second);
+            CancellationToken = new CancellationTokenSource();
+            RunningTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var triggerTime = DateTime.Today + TriggerHour - DateTime.Now;
+                    if (triggerTime < TimeSpan.Zero)
+                        triggerTime = triggerTime.Add(new TimeSpan(24, 0, 0));
+                    await Task.Delay(triggerTime, CancellationToken.Token);
+                    OnTimeTriggered?.Invoke();
+                }
+            }, CancellationToken.Token);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            CancellationToken?.Cancel();
+            CancellationToken?.Dispose();
+            CancellationToken = null;
+            RunningTask?.Dispose();
+            RunningTask = null;
+        }
+
+        /// <summary>
+        /// Triggers once every 24 hours on the specified time
+        /// </summary>
+        public event Action OnTimeTriggered;
+
+        /// <summary>
+        /// Finalized to ensure Dispose is called when out of scope
+        /// </summary>
+        ~DailyTrigger() => Dispose();
+    }
+}
