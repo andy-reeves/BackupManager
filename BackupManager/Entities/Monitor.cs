77bd8c201e674942a63824a1a18576a46695e503
Andy Reeves <andy@pb.eu>
2023-07-07 15:28:55 +0100

    Add new monitoring functionality

---
+++ BackupManager/Entities/Monitor.cs
@@ -0,0 +1,14 @@
+﻿namespace BackupManager.Entities
+{
+    public class Monitor
+    {
+        public string Url;
+        public int Timeout;
+        public string ProcessToKill;
+        public string ApplicationToStart;
+        public string ApplicationToStartArguments;
+        public string Name;
+        public int Port;
+        public string ServiceToRestart;
+    }
+}
