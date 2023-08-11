7ef94eeab1a84a17a1483a226b497c1a78fc238c
Andy Reeves <andy@pb.eu>
2023-08-11 18:55:08 +0100

    Check for filestoDelete in the root of the masterfolder alongside the indexfolder Update the sample xml file Add RegEx for Filename rules

---
+++ BackupManager/Entities/FileRule.cs
@@ -0,0 +1,20 @@
+﻿namespace BackupManager.Entities
+{
+    public class FileRule
+    {
+        /// <summary>
+        /// If the file path matches this then it must match FileRuleRegEx
+        /// </summary>
+        public string FileToMatchRegEx;
+
+        /// <summary>
+        /// If the file path matches FileToMatchRegEx then it must match this
+        /// </summary>
+        public string FileRuleRegEx;
+
+        /// <summary>
+        /// The message to display if the rule is not matched
+        /// </summary>
+        public string Message;
+    }
+}
