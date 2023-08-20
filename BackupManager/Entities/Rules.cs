b2867c61200f61c76099a672adb417e3ac603667
Andy Reeves <andy@pb.eu>
2023-08-20 11:33:20 +0100

    Rename the Monitor class Add the Rules class

---
+++ BackupManager/Entities/Rules.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="Rules.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager.Entities
+{
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    public class Rules
+    {
+        [XmlArrayItem("FileRule")]
+        public Collection<FileRule> FileRules;
+
+        public Rules()
+        {
+        }
+
+        public static Rules Load(string path)
+        {
+            Rules rules;
+            XmlSerializer serializer = new XmlSerializer(typeof(Rules));
+
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                rules = serializer.Deserialize(stream) as Rules;
+            }
+
+            return rules ?? null;
+        }
+    }
+}
