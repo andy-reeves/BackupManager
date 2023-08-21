8a3bf604990862d08501b67beb0a7cb9f672769c
Andy Reeves <andy@pb.eu>
2023-08-21 10:47:01 +0100

    Add ControlExtensions

---
+++ BackupManager/Extensions/ControlExtensions.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ControlExtensions.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager
+{
+    using System;
+    using System.Windows.Forms;
+
+    public static class ControlExtensions
+    {
+        public static void ThreadSafeCall(this Control control, Action method)
+        {
+            if (control.InvokeRequired)
+            {
+                _ = control.Invoke(method);
+            }
+            else
+            {
+                method();
+            }
+        }
+    }
+}
