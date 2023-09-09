ff9bda620a6561143984d529395f61930fc1a227
Andy Reeves <andy@pb.eu>
2023-09-09 21:12:12 +0100

    Add new config: MasterFoldersFileChangeWatchersONOFF MasterFoldersDaysBetweenFullScan MasterFoldersProcessChangesTimer MasterFoldersScanTimer MasterFolderScanMinimumAgeBeforeScanning Ensure sizeofCopy is >0 to avoid divide by zero Add ability to monitore master Folders for changes

---
+++ BackupManager/Entities/Folder.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="Folder.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager.Entities
+{
+    using System;
+
+    public class Folder
+    {
+        /// <summary>
+        /// The path of the folder.
+        /// </summary>
+        public string Path;
+
+        /// <summary>
+        /// The DateTime this folder last changed.
+        /// </summary>
+        public DateTime ModifiedDateTime;
+    }
+}
