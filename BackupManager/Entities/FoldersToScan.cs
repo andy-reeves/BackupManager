4c235aa9be3a27a198197bc38f942eb30454d754
Andy Reeves <andy@pb.eu>
2023-09-17 13:35:13 +0100

    Add FoldersToScan type for serializartion

---
+++ BackupManager/Entities/FoldersToScan.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="FoldersToScan.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager.Entities
+{
+    using System;
+
+    public class FoldersToScan
+    {
+        public string Path;
+
+        public DateTime Timestamp;
+
+        public FoldersToScan()
+        {
+        } 
+
+        public FoldersToScan(string scanFolder, DateTime dateTime)
+        {
+            Path = scanFolder;
+            Timestamp = dateTime;
+        }
+    }
+}
