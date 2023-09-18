355699f99f8ed0bb76a88fd42c98053259a8519d
Andy Reeves <andy@pb.eu>
2023-09-18 09:02:30 +0100

    Rename FoldersToScan to Folder

---
+++ BackupManager/Entities/Folder.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="FoldersToScan.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager.Entities
+{
+    using System;
+
+    /// <summary>
+    /// This class allows us to keep a Collection of FoldersToScan with the path and datetime it was last changed
+    /// </summary>
+    public class Folder : IEquatable<Folder>
+    {
+        /// <summary>
+        /// The path to the folder that changed
+        /// </summary>
+        public string Path;
+
+        /// <summary>
+        /// The Timestamp the folder was last changed
+        /// </summary>
+        public DateTime ModifiedDateTime;
+
+        public Folder()
+        {
+        }
+
+        public Folder(string scanFolder)
+        {
+            Path = scanFolder;
+            ModifiedDateTime = DateTime.Now;
+        }
+
+        public Folder(string scanFolder, DateTime dateTime)
+        {
+            Path = scanFolder;
+            ModifiedDateTime = dateTime;
+        }
+
+        public bool Equals(Folder other)
+        {
+            return null != other && Path == other.Path;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Folder);
+        }
+        public override int GetHashCode()
+        {
+            return Path.GetHashCode();
+        }
+    }
+}
