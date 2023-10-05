1ca05794daddce22c32bf48e6a523f4bbf720d21
Andy Reeves <andy@pb.eu>
2023-10-05 12:35:52 +0100

    Update ReSharper settings Add publish profile

--- BackupManager/Main.cs
+++ BackupManager/Main.cs
@@ -64,8 +64,9 @@ public partial class Main : Form
             InitializeComponent();
             TraceConfiguration.Register();
 #if DEBUG
-            Trace.Listeners.Add(new TextWriterTraceListener(
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "BackupManager_Trace.log"), "myListener"));
+            Trace.Listeners.Add(
+                new TextWriterTraceListener(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "BackupManager_Trace.log"),
+                    "myListener"));
 
             backupDiskTextBox.Text = "\\\\nas1\\assets1\\_Test\\BackupDisks\\backup 1001 parent";
 #else
