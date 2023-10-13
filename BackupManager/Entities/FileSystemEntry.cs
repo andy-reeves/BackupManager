0e854c342a388268e0b2cb8c9b9a80f761a28027
Andy Reeves <andy@pb.eu>
2023-10-13 12:17:11 +0100

    Add symbolic link entity Rename folder to FileSystemEntry

---
+++ BackupManager/Entities/FileSystemEntry.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="FileSystemEntry.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace BackupManager.Entities;
+
+/// <summary>
+///     This class allows us to keep a Collection of FileSystemEntry with the path and datetime it was last changed
+/// </summary>
+public class FileSystemEntry : IEquatable<FileSystemEntry>
+{
+    public FileSystemEntry() { }
+
+    public FileSystemEntry(string path) : this(path, DateTime.Now) { }
+
+    public FileSystemEntry(string path, DateTime dateTime)
+    {
+        Path = path;
+        ModifiedDateTime = dateTime;
+    }
+
+    /// <summary>
+    ///     The path to the FileSystemEntry that changed
+    /// </summary>
+
+    public string Path { get; set; }
+
+    private string InternalPath => Path;
+
+    /// <summary>
+    ///     The DateTime the FileSystemEntry was last changed
+    /// </summary>
+    public DateTime ModifiedDateTime { get; set; }
+
+    public bool Equals(FileSystemEntry other)
+    {
+        return null != other && Path == other.Path;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as FileSystemEntry);
+    }
+
+    public override int GetHashCode()
+    {
+        return InternalPath.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return Path;
+    }
+}
