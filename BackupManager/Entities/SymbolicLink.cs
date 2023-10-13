35127d65346d9477f47cd1aed42e0cdcbc14b5e3
Andy Reeves <andy@pb.eu>
2023-10-13 12:16:44 +0100

    Add symbolic link entity Rename folder to FileSystemEntry

---
+++ BackupManager/Entities/SymbolicLink.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="SymbolicLink.cs" company="Andy Reeves">
+//
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager.Entities;
+
+public class SymbolicLink
+{
+    public string RootFolder { get; set; }
+    public string FileDiscoveryRegEx { get; set; }
+    public string RelativePath { get; set; }
+    public string PathToTarget { get; set; }
+}
