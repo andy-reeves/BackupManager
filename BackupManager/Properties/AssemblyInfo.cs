831401e1fc2b2ca55299c8c6f955e38da7108f7a
Andy Reeves <andy@pb.eu>
2023-10-13 15:16:48 +0100

    Move assembly attributes into separate file

---
+++ BackupManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BackupManager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BackupManager")]
+[assembly: AssemblyCopyright("Copyright ©  Andy Reeves 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82c074d7-aa17-4c4d-af72-1d8f4416e85a")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+
+#if DEBUG
+[assembly: InternalsVisibleTo("TestProject")]
+#endif
