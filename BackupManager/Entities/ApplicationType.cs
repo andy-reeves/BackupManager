a551f296af13c8d7589e8e438649e5ffd54aadd2
Andy Reeves <andy@pb.eu>
2023-11-30 14:39:17 +0000

    Add scan details to the data we have. Start and end times

---
+++ BackupManager/Entities/ApplicationType.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ApplicationType.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace BackupManager.Entities;
+
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public enum ApplicationType
+{
+    Unknown = 0,
+
+    Plex = 1,
+
+    PlexPass = 2,
+
+    SABnzbd = 3,
+
+    Sonarr = 4,
+
+    Radarr = 5,
+
+    Bazarr = 6,
+
+    Prowlarr = 7
+}
