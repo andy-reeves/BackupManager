64dff186abe9a72914fc8a05b43c7efab79a01d4
Andy Reeves <andy@pb.eu>
2024-02-04 21:34:50 +0000

    Add missing files

---
+++ BackupManager/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="DateTimeExtensions.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace BackupManager.Extensions;
+
+internal static class DateTimeExtensions
+{
+    /// <summary>
+    ///     Convert datetime to UNIX time
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+
+    // ReSharper disable once UnusedMember.Global
+    internal static string ToUnixTime(this DateTime dateTime)
+    {
+        var dto = new DateTimeOffset(dateTime.ToUniversalTime());
+        return dto.ToUnixTimeSeconds().ToString();
+    }
+
+    /// <summary>
+    ///     Convert datetime to UNIX time including milliseconds
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    internal static string ToUnixTimeMilliseconds(this DateTime dateTime)
+    {
+        var dto = new DateTimeOffset(dateTime.ToUniversalTime());
+        return dto.ToUnixTimeMilliseconds().ToString();
+    }
+}
---
+++ BackupManager/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="IEnumerableExtensions.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BackupManager.Extensions;
+
+internal static class EnumerableExtensions
+{
+    public static bool Empty<TSource>(this IEnumerable<TSource> source)
+    {
+        return !source.Any();
+    }
+}
