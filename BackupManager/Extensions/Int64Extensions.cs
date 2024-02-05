b408160d80c2460015a12c6e4060d78832a597fc
Andy Reeves <andy@pb.eu>
2024-02-05 17:25:37 +0000

    Add Int64 Extensions

---
+++ BackupManager/Extensions/Int64Extensions.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="Int64Extensions.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+
+namespace BackupManager.Extensions;
+
+public static class Int64Extensions
+{
+    private static readonly string[] _sizeSuffixes = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+    public static string SizeSuffix(this long bytes)
+    {
+        switch (bytes)
+        {
+            case < 0:
+                return "-" + SizeSuffix(-bytes);
+            case 0:
+                return "0 B";
+        }
+        var mag = (int)Math.Log(bytes, Utils.BYTES_IN_ONE_KILOBYTE);
+        var adjustedSize = bytes / (decimal)Math.Pow(Utils.BYTES_IN_ONE_KILOBYTE, mag);
+        return string.Format(CultureInfo.InvariantCulture, "{0:n1} {1}", adjustedSize, _sizeSuffixes[mag]);
+    }
+}
