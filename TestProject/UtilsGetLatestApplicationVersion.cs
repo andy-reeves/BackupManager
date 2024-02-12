b65dff2a1664dae6647c558fc8d63c5e6fcd9d2f
Andy Reeves <andy@pb.eu>
2024-02-12 07:41:26 +0000

    Update tests

--- TestProject/UtilsGetLatestApplicationVersion.cs
+++ TestProject/UtilsGetLatestApplicationVersion.cs
@@ -49,7 +49,7 @@ public sealed class UtilsGetLatestApplicationVersion
 
         Assert.Equal(Utils.Config.PlexToken.HasValue() ? "1.40.0.7997" : "1.32.8.7639",
             Utils.GetLatestApplicationVersionNumber(ApplicationType.PlexPass));
-        Assert.Equal("1.13.3", Utils.GetLatestApplicationVersionNumber(ApplicationType.Prowlarr, "develop"));
-        Assert.Equal("5.3.5", Utils.GetLatestApplicationVersionNumber(ApplicationType.Radarr, "develop"));
+        Assert.Equal("1.14.0", Utils.GetLatestApplicationVersionNumber(ApplicationType.Prowlarr, "develop"));
+        Assert.Equal("5.3.6", Utils.GetLatestApplicationVersionNumber(ApplicationType.Radarr, "develop"));
     }
 }
