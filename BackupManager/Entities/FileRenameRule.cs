6be5807f860ca87672bb94991b6c938faeadbae8
Andy Reeves <andy@pb.eu>
2024-02-17 13:05:29 +0000

    Adding FileRenameRule to allow files to be renamed when scanning Added tests Updated tests Updated Config and schema

---
+++ BackupManager/Entities/FileRenameRule.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="FileRenameRule.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace BackupManager.Entities;
+
+[SuppressMessage("ReSharper", "MemberCanBeInternal")]
+[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+[SuppressMessage("ReSharper", "CollectionNeverUpdated.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public class FileRenameRule
+{
+    /// <summary>
+    ///     <FileDiscoveryRegEx>^.*\\_TV(\s\(non-tvdb\))?\\.*\.srt$</FileDiscoveryRegEx>
+    /// </summary>
+    public string FileDiscoveryRegex { get; set; }
+
+    /// <summary>
+    /// </summary>
+    public string Search { get; set; }
+
+    /// <summary>
+    /// </summary>
+    public string Replace { get; set; }
+}
