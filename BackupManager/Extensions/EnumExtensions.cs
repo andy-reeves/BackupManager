8ba5d50acbd36f884435ed9f7242d40e578dba2d
Andy Reeves <andy@pb.eu>
2024-03-04 12:59:45 +0000

    Adding new tests for AudioCodec check Add new string extensions Update naming rules Add FormatAudioCodec to Utils

---
+++ BackupManager/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace BackupManager.Extensions;
+
+internal static class EnumExtensions
+{
+    internal static string ToEnumMember<T>(this T value) where T : Enum
+    {
+        return typeof(T).GetTypeInfo().DeclaredMembers.SingleOrDefault(x => x.Name == value.ToString())
+            ?.GetCustomAttribute<EnumMemberAttribute>(false)?.Value;
+    }
+}
