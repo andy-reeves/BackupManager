36bdf487ac2a32a20a4f84dde9e09a7c3a9feedc
Andy Reeves <andy@pb.eu>
2024-03-17 22:23:32 +0000

    Add missing files Check first 10 fgrames of a file for HDR10 info Update tests

---
+++ BackupManager/Entities/VideoResolution.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="MovieVideoResolution.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace BackupManager.Entities;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+[SuppressMessage("ReSharper", "StringLiteralTypo")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+internal enum VideoResolution
+{
+    [EnumMember(Value = "")] Unknown = 0,
+
+    [EnumMember(Value = "480p")] R480p = 480,
+
+    [EnumMember(Value = "576p")] R576p = 576,
+
+    [EnumMember(Value = "720p")] R720p = 720,
+
+    [EnumMember(Value = "1080p")] R1080p = 1080,
+
+    [EnumMember(Value = "2160p")] R2160p = 2160
+}
