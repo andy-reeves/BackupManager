64c8a708d0861bbd1203bb071d1f43d4fae972c0
Andy Reeves <andy@pb.eu>
2024-03-20 11:23:22 +0000

    Move File utils into separate class Move Directory utils into spearate class

---
+++ BackupManager/Utils/Directory.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BackupManager;
+
+internal static partial class Utils
+{
+    internal static class Directory
+    {
+        internal static void EnsurePath(string directoryPath)
+        {
+            EnsureForFilePath(Path.Combine(directoryPath, "temp.txt"));
+        }
+
+        internal static void EnsureForFilePath(string filePath)
+        {
+            var directoryName = new FileInfo(filePath).DirectoryName;
+            if (directoryName != null) _ = System.IO.Directory.CreateDirectory(directoryName);
+        }
+
+        /// <summary>
+        ///     Deletes any empty directories in the directory specified and checks recursively all its subdirectories.
+        /// </summary>
+        /// <param name="directory">The directory to check</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <returns>An array of the directory paths that were removed</returns>
+        internal static IEnumerable<string> DeleteEmpty(string directory)
+        {
+            TraceIn();
+            ArgumentException.ThrowIfNullOrEmpty(directory, nameof(directory));
+            List<string> listOfDirectoriesDeleted = new();
+            DeleteEmpty(directory, listOfDirectoriesDeleted, directory);
+            return TraceOut(listOfDirectoriesDeleted.ToArray());
+        }
+
+        private static void DeleteEmpty(string directory, ICollection<string> list, string rootDirectory)
+        {
+            TraceIn(directory);
+
+            try
+            {
+                foreach (var subDirectory in System.IO.Directory.EnumerateDirectories(directory))
+                {
+                    DeleteEmpty(subDirectory, list, rootDirectory);
+                }
+
+                if (IsEmpty(directory))
+                {
+                    try
+                    {
+                        if (directory != rootDirectory)
+                        {
+                            Trace($"Deleting empty directory {directory}");
+                            list.Add(directory);
+                            _ = Delete(directory);
+                        }
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (DirectoryNotFoundException) { }
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+            TraceOut();
+        }
+
+        private static bool SetLastWriteUtc(string dirPath, DateTime lastWriteDate)
+        {
+            using var hDir = CreateFile(dirPath, FILE_ACCESS_GENERIC_READ | FILE_ACCESS_GENERIC_WRITE, FileShare.ReadWrite, IntPtr.Zero,
+                (FileMode)OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, IntPtr.Zero);
+            var lastWriteTime = lastWriteDate.ToFileTime();
+            return File.SetFileTime(hDir, IntPtr.Zero, IntPtr.Zero, ref lastWriteTime);
+        }
+
+        /// <summary>
+        ///     Checks the directory is writable
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>True if writable else False</returns>
+        internal static bool IsWritable(string path)
+        {
+            TraceIn(path);
+            if (!System.IO.Directory.Exists(path)) return TraceOut(false);
+
+            try
+            {
+                var lastWriteDate = System.IO.Directory.GetLastWriteTimeUtc(path);
+                var tempFile = Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + IS_DIRECTORY_WRITABLE_GUID + ".tmp";
+                using var fs = File.Create(Path.Combine(path, tempFile), 1, FileOptions.DeleteOnClose);
+                return TraceOut(SetLastWriteUtc(path, lastWriteDate));
+            }
+            catch
+            {
+                return TraceOut(false);
+            }
+        }
+
+        /// <summary>
+        ///     Checks to see if the directory specified is empty
+        /// </summary>
+        /// <param name="path">The directory to check</param>
+        /// <returns>
+        ///     True if the directory exists as a normal directory and it's empty. If it's a Symbolic link and the target is
+        ///     missing
+        ///     or the target is empty it returns True, otherwise False.
+        /// </returns>
+        internal static bool IsEmpty(string path)
+        {
+            if (!System.IO.Directory.Exists(path)) return false;
+            if (!IsSymbolicLink(path)) return !System.IO.Directory.EnumerateFileSystemEntries(path).Any();
+
+            var linkTarget = new FileInfo(path).LinkTarget;
+            return linkTarget != null && (!SymbolicLinkTargetExists(path) || !System.IO.Directory.GetFileSystemEntries(linkTarget).Any());
+        }
+
+        internal static bool Delete(string path, bool recursive = false)
+        {
+            TraceIn(path);
+            if (!System.IO.Directory.Exists(path)) return TraceOut(true);
+
+#if DIRECTORYDELETE
+            File.ClearFileAttribute(path, FileAttributes.ReadOnly);
+            System.IO.Directory.Delete(path, recursive);
+#else
+        LogWithPushover(BackupAction.General, PushoverPriority.High, $"DirectoryDelete with {path} - NOT DELETING", true, true);
+#endif
+            return TraceOut(true);
+        }
+
+        internal static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(sourceDirectory);
+            ArgumentException.ThrowIfNullOrEmpty(targetDirectory);
+            if (!System.IO.Directory.Exists(sourceDirectory)) throw new DirectoryNotFoundException();
+            if (System.IO.Directory.Exists(targetDirectory)) throw new NotSupportedException("Target Directory exists");
+
+            var diSource = new DirectoryInfo(sourceDirectory);
+            var diTarget = new DirectoryInfo(targetDirectory);
+            CopyAllFiles(diSource, diTarget);
+        }
+
+        private static void CopyAllFiles(DirectoryInfo source, DirectoryInfo target)
+        {
+            _ = System.IO.Directory.CreateDirectory(target.FullName);
+
+            foreach (var fi in source.GetFiles())
+            {
+                _ = fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            foreach (var diSourceSubDir in source.GetDirectories())
+            {
+                var nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAllFiles(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        internal static bool Exists(string directory)
+        {
+            return System.IO.Directory.Exists(directory);
+        }
+
+        internal static DirectoryInfo CreateDirectory(string path)
+        {
+            return System.IO.Directory.CreateDirectory(path);
+        }
+    }
+}
---
+++ BackupManager/Utils/File.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using BackupManager.Extensions;
+using BackupManager.Properties;
+
+using Microsoft.Win32.SafeHandles;
+
+namespace BackupManager;
+
+internal static partial class Utils
+{
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    internal static partial class File
+    {
+        [LibraryImport("kernel32.dll", EntryPoint = "SetFileTime", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static partial bool SetFileTime(SafeFileHandle hFile, IntPtr lpCreationTimeUnused, IntPtr lpLastAccessTimeUnused,
+            ref long lpLastWriteTime);
+
+        /// <summary>
+        ///     Checks the path is a video file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>False if it's not a video file, True if it's a video file. Exception if path is null or empty</returns>
+        internal static bool IsVideo(string path)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(path);
+            return Path.GetExtension(path).ToLowerInvariant().ContainsAny(_videoExtensions);
+        }
+
+        /// <summary>
+        ///     Calculates the short MD5 hash of the file.
+        /// </summary>
+        /// <param name="stream">
+        ///     The stream.
+        /// </param>
+        /// <param name="size">
+        ///     The size.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="string" />.
+        /// </returns>
+        internal static string GetShortMd5HashFromFile(FileStream stream, long size)
+        {
+            if (stream == null) return null;
+            if (size <= 0) return string.Empty;
+
+            byte[] startBlock;
+            byte[] middleBlock = null;
+            byte[] endBlock = null;
+
+            if (size >= START_BLOCK_SIZE + MIDDLE_BLOCK_SIZE + END_BLOCK_SIZE)
+            {
+                var startDownloadPositionForEndBlock = size - END_BLOCK_SIZE;
+                var startDownloadPositionForMiddleBlock = size / 2;
+                startBlock = GetLocalFileByteArray(stream, 0, START_BLOCK_SIZE);
+                middleBlock = GetLocalFileByteArray(stream, startDownloadPositionForMiddleBlock, MIDDLE_BLOCK_SIZE);
+                endBlock = GetLocalFileByteArray(stream, startDownloadPositionForEndBlock, END_BLOCK_SIZE);
+            }
+            else
+                startBlock = GetLocalFileByteArray(stream, 0, size);
+            return CreateHashForByteArray(startBlock, middleBlock, endBlock);
+        }
+
+        /// <summary>
+        ///     Gets an MD5 hash of the first 16K, the 16K from the middle and last 16K of a file.
+        /// </summary>
+        /// <param name="path">
+        ///     The local file name.
+        /// </param>
+        /// <returns>
+        ///     An MD5 hash of the file or null if File doesn't exist or string.Empty if it has no size.
+        /// </returns>
+        internal static string GetShortMd5Hash(string path)
+        {
+            TraceIn(path);
+            ArgumentException.ThrowIfNullOrEmpty(path);
+            if (!Exists(path)) throw new FileNotFoundException(Resources.FileNotFound, path);
+
+            var size = new FileInfo(path).Length;
+            if (size == 0) return string.Empty;
+
+            byte[] startBlock;
+            byte[] middleBlock = null;
+            byte[] endBlock = null;
+
+            if (size >= START_BLOCK_SIZE + MIDDLE_BLOCK_SIZE + END_BLOCK_SIZE)
+            {
+                var startDownloadPositionForEndBlock = size - END_BLOCK_SIZE;
+                var startDownloadPositionForMiddleBlock = size / 2;
+                startBlock = GetLocalFileByteArray(path, 0, START_BLOCK_SIZE);
+                middleBlock = GetLocalFileByteArray(path, startDownloadPositionForMiddleBlock, MIDDLE_BLOCK_SIZE);
+                endBlock = GetLocalFileByteArray(path, startDownloadPositionForEndBlock, END_BLOCK_SIZE);
+            }
+            else
+                startBlock = GetLocalFileByteArray(path, 0, size);
+            return TraceOut(CreateHashForByteArray(startBlock, middleBlock, endBlock));
+        }
+
+        internal static bool SetLastWriteTime(string fileName, DateTime writeTimeToUse)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(fileName, nameof(fileName));
+            if (!Exists(fileName)) throw new FileNotFoundException(Resources.FileNotFound, fileName);
+
+            FileInfo fileInfo = new(fileName);
+
+            try
+            {
+                fileInfo.LastWriteTime = writeTimeToUse;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal static DateTime GetLastWriteTime(string fileName)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(fileName, nameof(fileName));
+            if (!Exists(fileName)) throw new FileNotFoundException(Resources.FileNotFound, fileName);
+
+            FileInfo fileInfo = new(fileName);
+            DateTime returnValue;
+
+            try
+            {
+                returnValue = fileInfo.LastWriteTime;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // Sometimes the file doesn't have a valid LastWriteTime 
+                //If we cant read the LastWriteTime then copy the LastAccessTime over it and use that instead
+                fileInfo.LastWriteTime = fileInfo.LastAccessTime;
+                returnValue = fileInfo.LastWriteTime;
+            }
+            return returnValue;
+        }
+
+        internal static long GetLength(string fileName)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(fileName, nameof(fileName));
+            if (!Exists(fileName)) throw new FileNotFoundException(Resources.FileNotFound, fileName);
+
+            return new FileInfo(fileName).Length;
+        }
+
+        /// <summary>
+        ///     Checks the path is a subtitles file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>False if it's not a subtitles file, True if it's a subtitles file. Exception if path is null or empty</returns>
+        internal static bool IsSubtitles(string path)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(path);
+            return Path.GetExtension(path).ToLowerInvariant().ContainsAny(_subtitlesExtensions);
+        }
+
+        internal static bool IsSpecialFeature(string path)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(path);
+            return path.ContainsAny(_specialFeatures);
+        }
+
+        private static FileAttributes RemoveAttribute(FileAttributes attributes, FileAttributes attributesToRemove)
+        {
+            return attributes & ~attributesToRemove;
+        }
+
+        /// <summary>
+        ///     Returns True if FileName is accessible (not locked) by another process
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>True if not locked or False if locked</returns>
+        internal static bool IsAccessible(string path)
+        {
+            try
+            {
+                FileStream fileStream = new(path, FileMode.Open, FileAccess.Read);
+                fileStream.Close();
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///     Clears the attribute from the file if it were set.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="attributeToRemove"></param>
+        internal static void ClearFileAttribute(string path, FileAttributes attributeToRemove)
+        {
+            TraceIn(path, attributeToRemove);
+            var attributes = GetAttributes(path);
+
+            if ((attributes & attributeToRemove) == attributeToRemove)
+            {
+                attributes = RemoveAttribute(attributes, attributeToRemove);
+                SetAttributes(path, attributes);
+            }
+            TraceOut();
+        }
+
+        internal static bool IsDolbyVisionProfile5(string path)
+        {
+            TraceIn(path);
+            ArgumentException.ThrowIfNullOrEmpty(path);
+            ArgumentException.ThrowIfNullOrEmpty(path);
+            if (!IsVideo(path) || !VideoFileIsDolbyVision(path)) return TraceOut(false);
+
+            if (!Exists(path)) throw new FileNotFoundException(Resources.FileNotFound, path);
+
+            var info = new VideoFileInfoReader().GetMediaInfo(path);
+
+            // ReSharper disable once StringLiteralTypo
+            return info == null
+                ? throw new ApplicationException("Unable to load ffprobe.exe")
+                : TraceOut(info is { DoviConfigurationRecord.DvProfile: 5 });
+        }
+
+        /// <summary>
+        ///     Copies an existing file to a new file asynchronously. Overwriting a file of the same name is not allowed. Ensures
+        ///     the destination folder exists too.
+        /// </summary>
+        /// <param name="sourceFileName">The file to copy.</param>
+        /// <param name="destFileName">The name of the destination file. This cannot be a directory or an existing file.</param>
+        /// <param name="ct"></param>
+        /// <returns>True if copy was successful or False</returns>
+        internal static bool Copy(string sourceFileName, string destFileName, CancellationToken ct)
+        {
+            TraceIn(sourceFileName, destFileName);
+            if (destFileName == null || sourceFileName == null) return false;
+
+            if (sourceFileName.Length > 256) throw new NotSupportedException($"Source file name {sourceFileName} exceeds 256 characters");
+            if (destFileName.Length > 256) throw new NotSupportedException($"Destination file name {destFileName} exceeds 256 characters");
+            if (Exists(destFileName)) throw new NotSupportedException("Destination file already exists");
+
+            Directory.EnsureForFilePath(destFileName);
+            if (ct.IsCancellationRequested) ct.ThrowIfCancellationRequested();
+
+            // ReSharper disable once CommentTypo
+            // we create the destination file so xcopy knows it's a file and can copy over it
+            WriteAllText(destFileName, "Temp file"); // hash of this is 88f85bbea58fbff062050bcb2d2aafcf
+
+            CopyProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "xcopy",
+                    Arguments = $"/H /Y \"{sourceFileName}\" \"{destFileName}\""
+                }
+            };
+            if (!CopyProcess.Start()) return TraceOut(false);
+
+            try
+            {
+                var processId = CopyProcess.Id;
+
+                // CopyProcess may be null here already if the file was very small
+                CopyProcess.WaitForExit();
+
+                // We wait because otherwise lots of copy processes will start at once
+                // WaitForExit sometimes returns too early (especially for small files)
+                // So we then wait until the processID has gone completely
+                // This will throw ArgumentException if the process has stopped already
+
+                // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                while (Process.GetProcessById(processId) != null)
+                {
+                    Wait(1);
+                }
+
+                // We never exit here
+                return TraceOut(false);
+            }
+
+            // ArgumentException is thrown when the process actually stops
+            // Then we wait until the file is actually not locked anymore and the hash codes match
+            // InvalidOperationException is thrown if we access the Process and its already completed
+            catch (Exception ex) when (ex is ArgumentException or InvalidOperationException)
+            {
+                var hashSource = GetShortMd5Hash(sourceFileName);
+
+                while (!IsAccessible(destFileName) || GetShortMd5Hash(destFileName) != hashSource)
+                {
+                    Wait(1);
+                    if (ct.IsCancellationRequested) ct.ThrowIfCancellationRequested();
+                }
+                return TraceOut(true);
+            }
+        }
+
+        /// <summary>
+        ///     Deletes the file specified if it exists even if it was readonly. Returns true if the file doesn't exist or if it
+        ///     was deleted successfully
+        /// </summary>
+        /// <param name="path"></param>
+        internal static bool Delete(string path)
+        {
+            TraceIn(path);
+            if (!Exists(path)) return TraceOut(true);
+
+#if FILEDELETE
+            ClearFileAttribute(path, FileAttributes.ReadOnly);
+            System.IO.File.Delete(path);
+#else
+        LogWithPushover(BackupAction.General, PushoverPriority.High, $"FileDelete with {path} - NOT DELETING", true, true);
+#endif
+            return TraceOut(true);
+        }
+
+        /// <summary>
+        ///     Moves a specified file to a new location, providing the option to specify a new file name. Ensures the destination
+        ///     folder exists too.
+        /// </summary>
+        /// <param name="sourceFileName">The name of the file to move. Can include a relative or absolute path.</param>
+        /// <param name="destFileName">The new path and name for the file.</param>
+        /// <returns>True if successfully renamed otherwise False</returns>
+        internal static bool Move(string sourceFileName, string destFileName)
+        {
+            TraceIn(sourceFileName, destFileName);
+#if FILEMOVE
+            Directory.EnsureForFilePath(destFileName);
+            System.IO.File.Move(sourceFileName, destFileName);
+#else
+        LogWithPushover(BackupAction.General, PushoverPriority.High, $"FileMove with {sourceFileName} to {destFileName} - NOT MOVING", true, true);
+#endif
+            return TraceOut(true);
+        }
+
+        internal static void AppendAllText(string filePath, string contents)
+        {
+            System.IO.File.AppendAllText(filePath, contents);
+        }
+
+        internal static bool Exists(string path)
+        {
+            return System.IO.File.Exists(path);
+        }
+
+        internal static string ReadAllText(string path)
+        {
+            return System.IO.File.ReadAllText(path);
+        }
+
+        internal static void WriteAllText(string path, string contents)
+        {
+            System.IO.File.WriteAllText(path, contents);
+        }
+
+        internal static FileAttributes GetAttributes(string path)
+        {
+            return System.IO.File.GetAttributes(path);
+        }
+
+        internal static void SetAttributes(string path, FileAttributes fileAttributes)
+        {
+            System.IO.File.SetAttributes(path, fileAttributes);
+        }
+
+        internal static Stream OpenRead(string path)
+        {
+            return System.IO.File.OpenRead(path);
+        }
+
+        internal static void AppendAllLines(string path, string[] contents)
+        {
+            System.IO.File.AppendAllLines(path, contents);
+        }
+
+        internal static FileStream Create(string path, int bufferSize, FileOptions options)
+        {
+            return System.IO.File.Create(path, bufferSize, options);
+        }
+
+        internal static void Copy(string sourceFileName, string destFileName)
+        {
+            System.IO.File.Copy(sourceFileName, destFileName);
+        }
+    }
+}
