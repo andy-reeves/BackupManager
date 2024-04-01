f104fe07adf3de25906c168ac28b3a947871e0f5
Andy Reeves <andy@pb.eu>
2024-04-01 14:17:12 +0100

    Update checkconnecteddiskrepaeater so its scan the pending changes directories first before copiying

--- BackupManager/Main.ConnectedDisk.cs
+++ BackupManager/Main.ConnectedDisk.cs
@@ -49,6 +49,12 @@ internal sealed partial class Main
 
             while (!ct.IsCancellationRequested)
             {
+                ReadyToScan(new FileSystemWatcherEventArgs(mediaBackup.Watcher.DirectoriesToScan.ToArray()), SearchOption.AllDirectories, true,
+                    ct);
+
+                // Empty the DirectoriesToScan because we've processed all of them now
+                // we do it here so if we get cancelled before this we leave the directories ready to scan for next time
+                mediaBackup.Watcher.DirectoriesToScan.Clear();
                 var lastBackupDiskChecked = CheckConnectedDisk(true, ct);
 
                 if (lastBackupDiskChecked == null)
@@ -225,7 +231,7 @@ internal sealed partial class Main
         // as it's an extra file on the backup disk
         if (deleteExtraFiles)
         {
-            Utils.LogWithPushover(BackupAction.CheckBackupDisk, PushoverPriority.High,
+            Utils.LogWithPushover(BackupAction.CheckBackupDisk, PushoverPriority.Normal,
                 $"Extra file {backupDiskFileFullPath} on backup disk {disk.Name} now deleted");
             _ = Utils.File.Delete(backupDiskFileFullPath);
         }
