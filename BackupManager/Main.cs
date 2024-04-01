fe198f76b00dc10b54bcd1ec31770cdf4871d9cb
Andy Reeves <andy@pb.eu>
2024-04-01 15:00:29 +0100

    Remove the scanned directories when completed

--- BackupManager/Main.cs
+++ BackupManager/Main.cs
@@ -1165,11 +1165,15 @@ internal sealed partial class Main : Form
         if (longRunningActionExecutingRightNow) return;
 
         DisableControlsForAsyncTasks(ct);
-        ReadyToScan(new FileSystemWatcherEventArgs(mediaBackup.Watcher.DirectoriesToScan.ToArray()), SearchOption.AllDirectories, true, ct);
+        var dirsToScan = mediaBackup.Watcher.DirectoriesToScan.ToArray();
+        ReadyToScan(new FileSystemWatcherEventArgs(dirsToScan), SearchOption.AllDirectories, true, ct);
 
         // Empty the DirectoriesToScan because we've processed all of them now
         // we do it here so if we get cancelled before this we leave the directories ready to scan for next time
-        mediaBackup.Watcher.DirectoriesToScan.Clear();
+        foreach (var a in dirsToScan)
+        {
+            _ = mediaBackup.Watcher.DirectoriesToScan.Remove(a);
+        }
         ResetAllControls();
         UpdateUI_Tick(null, null);
         UpdateMediaFilesCountDisplay();
