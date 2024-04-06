acb95952f3c373adef6d451e11c2b5ae6241d4bf
Andy Reeves <andy@pb.eu>
2024-04-06 12:31:13 +0100

    Update tests

--- TestProject/MovieBackupFileTests.cs
+++ TestProject/MovieBackupFileTests.cs
@@ -21,7 +21,6 @@ public sealed class MovieBackupFileTests
     [InlineData("File13 (2024) [Remux-1080p][DTS-HD MA 5.1][h264].mkv", false, true, "File13 (2024) [Remux-1080p][DTS-HD MA 5.1][h264].mkv")]
     [InlineData("File14 (2024) [WEBDL-1080p][EAC3 5.1][h264].mkv", false, true, "File14 (2024) [WEBDL-1080p][EAC3 5.1][h265].mkv")]
     [InlineData("Avengers Infinity War (2018) {tmdb-299536} [Remux-2160p][HDR10][TrueHD Atmos 7.1][h265].mkv", false, true, "Avengers Infinity War (2018) {tmdb-299536} [Remux-2160p][HDR10][TrueHD Atmos 7.1][h265].mkv")]
-    [InlineData(@"\\nas3\assets3\_Movies\Joker (2019)\Joker (2019) {tmdb-475557} [Remux-2160p][HDR10][TrueHD Atmos 7.1][h265].mkv", true, true, "Joker (2019) {tmdb-475557} [Remux-2160p][HDR10][TrueHD Atmos 7.1][h265].mkv")]
     public void MovieRefreshInfoTests(string sourceFileName, bool validDirectoryName, bool validFileName, string expectedFileName)
     {
         string fileName;
