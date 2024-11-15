161bdec2e09c72d4ba82ea58f9e6b4a73abdf787
Andy Reeves <andy@pb.eu>
2024-11-15 13:00:17 +0000

    Update HasAny for bitwise flags checking Remove unused references

---
+++ BackupManager/Utils/Enums.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+// ReSharper disable MemberCanBeInternal
+namespace BackupManager;
+
+/// <summary>
+///     Provides a set of static methods for use with enum types. Much of
+///     what's available here is already in System.Enum, but this class
+///     provides a strongly typed API.
+/// </summary>
+public static class Enums
+{
+    /// <summary>
+    ///     Returns an array of values in the enum.
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <returns>An array of values in the enum</returns>
+    public static T[] GetValuesArray<T>() where T : struct
+    {
+        return (T[])Enum.GetValues(typeof(T));
+    }
+
+    /// <summary>
+    ///     Returns the values for the given enum as an immutable list.
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
+    public static IList<T> GetValues<T>() where T : struct
+    {
+        return EnumInternals<T>.Values;
+    }
+
+    /// <summary>
+    ///     Returns an array of names in the enum.
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <returns>An array of names in the enum</returns>
+    public static string[] GetNamesArray<T>() where T : struct
+    {
+        return Enum.GetNames(typeof(T));
+    }
+
+    /// <summary>
+    ///     Returns the names for the given enum as an immutable list.
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <returns>An array of names in the enum</returns>
+    public static IList<string> GetNames<T>() where T : struct
+    {
+        return EnumInternals<T>.Names;
+    }
+
+    /// <summary>
+    ///     Checks whether the value is a named value for the type.
+    /// </summary>
+    /// <remarks>
+    ///     For flags enums, it is possible for a value to be a valid
+    ///     combination of other values without being a named value
+    ///     in itself. To test for this possibility, use IsValidCombination.
+    /// </remarks>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <param name="value">Value to test</param>
+    /// <returns>True if this value has a name, False otherwise.</returns>
+    public static bool IsNamedValue<T>(this T value) where T : struct
+    {
+        // TODO: Speed this up for big enums
+        return GetValues<T>().Contains(value);
+    }
+
+    /// <summary>
+    ///     Returns the description for the given value,
+    ///     as specified by DescriptionAttribute, or null
+    ///     if no description is present.
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <param name="item">Value to fetch description for</param>
+    /// <returns>
+    ///     The description of the value, or null if no description
+    ///     has been specified (but the value is a named value).
+    /// </returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     <paramref name="item" />
+    ///     is not a named member of the enum
+    /// </exception>
+    public static string GetDescription<T>(this T item) where T : struct
+    {
+        string description;
+        if (EnumInternals<T>.ValueToDescriptionMap.TryGetValue(item, out description)) return description;
+
+        throw new ArgumentOutOfRangeException("item");
+    }
+
+    /// <summary>
+    ///     Attempts to find a value with the given description.
+    /// </summary>
+    /// <remarks>
+    ///     More than one value may have the same description. In this unlikely
+    ///     situation, the first value with the specified description is returned.
+    /// </remarks>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <param name="description">Description to find</param>
+    /// <param name="value">Enum value corresponding to given description (on return)</param>
+    /// <returns>
+    ///     True if a value with the given description was found,
+    ///     false otherwise.
+    /// </returns>
+    public static bool TryParseDescription<T>(string description, out T value) where T : struct
+    {
+        return EnumInternals<T>.DescriptionToValueMap.TryGetValue(description, out value);
+    }
+
+    /// <summary>
+    ///     Parses the name of an enum value.
+    /// </summary>
+    /// <remarks>
+    ///     This method only considers named values: it does not parse comma-separated
+    ///     combinations of flags enums.
+    /// </remarks>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <returns>The parsed value</returns>
+    /// <exception cref="ArgumentException">The name could not be parsed.</exception>
+    public static T ParseName<T>(string name) where T : struct
+    {
+        T value;
+        if (!TryParseName(name, out value)) throw new ArgumentException("Unknown name", "name");
+
+        return value;
+    }
+
+    /// <summary>
+    ///     Attempts to find a value for the specified name.
+    ///     Only names are considered - not numeric values.
+    /// </summary>
+    /// <remarks>
+    ///     If the name is not parsed, <paramref name="value" /> will
+    ///     be set to the zero value of the enum. This method only
+    ///     considers named values: it does not parse comma-separated
+    ///     combinations of flags enums.
+    /// </remarks>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <param name="name">Name to parse</param>
+    /// <param name="value">Enum value corresponding to given name (on return)</param>
+    /// <returns>Whether the parse attempt was successful or not</returns>
+    public static bool TryParseName<T>(string name, out T value) where T : struct
+    {
+        // TODO: Speed this up for big enums
+        var index = EnumInternals<T>.Names.IndexOf(name);
+
+        if (index == -1)
+        {
+            value = default;
+            return false;
+        }
+        value = EnumInternals<T>.Values[index];
+        return true;
+    }
+
+    /// <summary>
+    ///     Returns the underlying type for the enum
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
+    /// <returns>The underlying type (Byte, Int32 etc) for the enum</returns>
+    public static Type GetUnderlyingType<T>() where T : struct
+    {
+        return EnumInternals<T>.UnderlyingType;
+    }
+}
