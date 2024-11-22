066b66eaa1954203fdd70e4663488676503638c8
Andy Reeves <andy@pb.eu>
2024-11-22 10:50:21 +0000

    Add missing files

---
+++ BackupManager/Entities/FileSystemEntryType.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="FileSystemEntryType.cs" company="Andy Reeves">
+// 
+//  </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BackupManager.Entities;
+
+/// <summary>
+///     Type of file system object
+/// </summary>
+internal enum FileSystemEntryType
+{
+    /// <summary>
+    ///     File and Directory do not exist
+    /// </summary>
+    Missing = 0,
+
+    /// <summary>
+    ///     A file.
+    /// </summary>
+    File,
+
+    /// <summary>
+    ///     A directory
+    /// </summary>
+    Directory
+}
