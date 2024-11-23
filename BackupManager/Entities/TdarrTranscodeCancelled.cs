5091f24fb1acd997578c2371f84101f4b6f3a25d
Andy Reeves <andy@pb.eu>
2024-11-23 11:12:44 +0000

    ReSharper fixes

--- BackupManager/Entities/TdarrTranscodeCancelled.cs
+++ BackupManager/Entities/TdarrTranscodeCancelled.cs
@@ -2,72 +2,71 @@
 
 using CsvHelper.Configuration.Attributes;
 
-namespace BackupManager.Entities
+namespace BackupManager.Entities;
+
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+internal class TdarrTranscodeCancelled
 {
-    [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
-    internal class TdarrTranscodeCancelled
-    {
-        [Name("_id")] public string Id { get; set; }
+    [Name("_id")] public string Id { get; set; }
 
-        [Name("file")] public string File { get; set; }
+    [Name("file")] public string File { get; set; }
 
-        [Name("DB")] public string Db { get; set; }
+    [Name("DB")] public string Db { get; set; }
 
-        // 
-        [Name("footprintId")] public string FootprintId { get; set; }
+    // 
+    [Name("footprintId")] public string FootprintId { get; set; }
 
-        [Name("hasClosedCaptions")] public string HasClosedCaptions { get; set; }
+    [Name("hasClosedCaptions")] public string HasClosedCaptions { get; set; }
 
-        [Name("container")] public string Container { get; set; }
+    [Name("container")] public string Container { get; set; }
 
-        [Name("scannerReads")] public string ScannerReads { get; set; }
+    [Name("scannerReads")] public string ScannerReads { get; set; }
 
-        [Name("ffProbeData")] public string FfProbeData { get; set; }
+    [Name("ffProbeData")] public string FfProbeData { get; set; }
 
-        [Name("file_size")] public string FileSize { get; set; }
+    [Name("file_size")] public string FileSize { get; set; }
 
-        [Name("video_resolution")] public string VideoResolution { get; set; }
+    [Name("video_resolution")] public string VideoResolution { get; set; }
 
-        [Name("fileMedium")] public string FileMedium { get; set; }
+    [Name("fileMedium")] public string FileMedium { get; set; }
 
-        [Name("video_codec_name")] public string VideoCodecName { get; set; }
+    [Name("video_codec_name")] public string VideoCodecName { get; set; }
 
-        [Name("audio_codec_name")] public string AudioCodecName { get; set; }
+    [Name("audio_codec_name")] public string AudioCodecName { get; set; }
 
-        [Name("lastPluginDetails")] public string LastPluginDetails { get; set; }
+    [Name("lastPluginDetails")] public string LastPluginDetails { get; set; }
 
-        [Name("createdAt")] public string CreatedAt { get; set; }
+    [Name("createdAt")] public string CreatedAt { get; set; }
 
-        [Name("bit_rate")] public string BitRate { get; set; }
+    [Name("bit_rate")] public string BitRate { get; set; }
 
-        [Name("duration")] public string Duration { get; set; }
+    [Name("duration")] public string Duration { get; set; }
 
-        [Name("statSync")] public string StatSync { get; set; }
+    [Name("statSync")] public string StatSync { get; set; }
 
-        [Name("HealthCheck")] public string HealthCheck { get; set; }
+    [Name("HealthCheck")] public string HealthCheck { get; set; }
 
-        [Name("TranscodeDecisionMaker")] public string TranscodeDecisionMaker { get; set; }
+    [Name("TranscodeDecisionMaker")] public string TranscodeDecisionMaker { get; set; }
 
-        [Name("lastHealthCheckDate")] public string LastHealthCheckDate { get; set; }
+    [Name("lastHealthCheckDate")] public string LastHealthCheckDate { get; set; }
 
-        [Name("holdUntil")] public string HoldUntil { get; set; }
+    [Name("holdUntil")] public string HoldUntil { get; set; }
 
-        [Name("lastTranscodeDate")] public string LastTranscodeDate { get; set; }
+    [Name("lastTranscodeDate")] public string LastTranscodeDate { get; set; }
 
-        [Name("bumped")] public string Bumped { get; set; }
+    [Name("bumped")] public string Bumped { get; set; }
 
-        [Name("history")] public string History { get; set; }
+    [Name("history")] public string History { get; set; }
 
-        [Name("oldSize")] public string OldSize { get; set; }
+    [Name("oldSize")] public string OldSize { get; set; }
 
-        [Name("newSize")] public string NewSize { get; set; }
+    [Name("newSize")] public string NewSize { get; set; }
 
-        [Name("newVsOldRatio")] public string NewVsOldRatio { get; set; }
+    [Name("newVsOldRatio")] public string NewVsOldRatio { get; set; }
 
-        [Name("videoStreamIndex")] public string VideoStreamIndex { get; set; }
+    [Name("videoStreamIndex")] public string VideoStreamIndex { get; set; }
 
-        [Name("lastUpdate")] public string LastUpdate { get; set; }
-    }
+    [Name("lastUpdate")] public string LastUpdate { get; set; }
 }
